27e13e2a4b55dad2869169146ab9d093f2073fa1
0genina0 <geni23cs@student.ju.se>
2024-04-05 11:46:24 +0200

    started css styling

--- todo.js
+++ todo.js
@@ -1,12 +1,41 @@
 let inputField = document.getElementById("inputField");
 let submitButton = document.getElementById("submitButton");
-let displayArea = document.getElementById("displayArea");
+let listContainer = document.getElementById("listContainer");
 
+window.addEventListener("load", function() {
+    let storedData = localStorage.getItem("listItems");
+    if (storedData) {
+        listContainer.innerHTML = storedData;
+    }
+});
 
 submitButton.addEventListener("click", function() {
     let inputData = inputField.value;
-    let jsonData = {
-        "inputData": inputData
-    };
-    displayArea.jsonData = JSON.stringify(jsonData);
+    
+    inputField.textContent = "Enter Task";
+
+    let listItem = document.createElement("li");
+    listItem.textContent = inputData;
+
+    let removeButton = document.createElement("button");
+    removeButton.className = "x-icon";
+    removeButton.addEventListener("click", function(){
+        listItem.remove();
+        saveList();
+    }); 
+
+    let strikeButton = document.createElement("button");
+    strikeButton.className = "checkmark";
+    strikeButton.addEventListener("click", function(){
+        listItem.style.textDecoration = "line-through";
+        saveList();
+    }); 
+
+    listItem.appendChild(removeButton);
+    listItem.appendChild(strikeButton);
+    listContainer.appendChild(listItem);
 });
+
+function saveList(){
+    localStorage.setItem("listItems",listContainer.innerHTML);
+}
